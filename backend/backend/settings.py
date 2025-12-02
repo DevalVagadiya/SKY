25ca21b35188aa425eed0689321e723c1e594c2e
DevalVagadiya <devalvagadiya2005@gmail.com>
2025-12-02 22:48:53 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,6 +137,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # React dev server
+    "https://sky-weld.vercel.app/",  # React dev server
 ]
 CORS_ALLOW_CREDENTIALS = True
