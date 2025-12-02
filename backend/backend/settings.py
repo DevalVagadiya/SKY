a835259f7fb3446d624d9f1f4b14a091df0e936c
DevalVagadiya <devalvagadiya2005@gmail.com>
2025-12-02 22:45:18 +0530

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+@&otwuuhu8iqc8udbdqy1bnbepofh4wl%)q@uwg=e&x**c!e1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sky-backend.onrender.com", "localhost"]
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
 # Default primary key field type
